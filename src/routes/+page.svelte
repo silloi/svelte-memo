6d0834c4383c13af9957eac7e1c174f294b83ec9
silloi <silloi1993@gmail.com>
2023-10-08 12:32:45 +0900

    format

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,31 +1,29 @@
 <script lang="ts">
-  import Drawer, { AppContent, Content } from '@smui/drawer';
+	import Drawer, { AppContent, Content } from '@smui/drawer';
 	import MemoList from '$lib/MemoList.svelte';
 </script>
 
 <div class="drawer-container">
+	<Drawer>
+		<Content>
+			<MemoList />
+		</Content>
+	</Drawer>
 
-<Drawer>
-  <Content>
-		<MemoList />
-	</Content>
-</Drawer>
-
-<AppContent class="main-content">
-	<main class="mainpanel">
-		<div class="w-full">hi!</div>
-  </main>
-</AppContent>
-
+	<AppContent class="main-content">
+		<main class="mainpanel">
+			<div class="w-full">hi!</div>
+		</main>
+	</AppContent>
 </div>
 
 <style>
-  .drawer-container {
-    position: relative;
-    display: flex;
-    height: 100%;
-    overflow-y: hidden;
-  }
+	.drawer-container {
+		position: relative;
+		display: flex;
+		height: 100%;
+		overflow-y: hidden;
+	}
 
 	/* .main-content {
 		flex: 1;
