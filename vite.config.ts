187555c2d3cd2cd96b8f4a2472a63859f2cae97c
silloi <silloi1993@gmail.com>
2023-10-08 20:00:27 +0900

    add manifest

--- vite.config.ts
+++ vite.config.ts
@@ -7,6 +7,25 @@ export default defineConfig({
 		sveltekit(),
 		SvelteKitPWA({
 			manifest: {
+				name: 'Svelte Menos',
+				short_name: 'Menos',
+				description: 'The missing link between memos and notes',
+				display: 'standalone',
+				background_color: '#ffffff',
+				icons: [
+					{
+						src: './favicon-192x192.png',
+						sizes: '192x192',
+						type: 'image/png',
+						purpose: 'any maskable'
+					},
+					{
+						src: './favicon-512x512.png',
+						sizes: '512x512',
+						type: 'image/png',
+						purpose: 'any maskable'
+					}
+				],
 				share_target: {
 					action: '/new',
 					method: 'GET',
