0329d328acf754fd812b957e4081e39fc9280f09
silloi <silloi1993@gmail.com>
2023-10-08 21:54:02 +0900

    ciao @vite-pwa/sveltekit

--- vite.config.ts
+++ vite.config.ts
@@ -1,46 +1,6 @@
 import { sveltekit } from '@sveltejs/kit/vite';
-import { SvelteKitPWA } from '@vite-pwa/sveltekit';
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-	plugins: [
-		sveltekit(),
-		SvelteKitPWA({
-			registerType: 'autoUpdate',
-			manifest: {
-				name: 'Svelte Menos',
-				short_name: 'Menos',
-				description: 'The missing link between memos and notes',
-				display: 'standalone',
-				start_url: './app.html',
-				background_color: '#ffffff',
-				icons: [
-					{
-						src: '/icon-192x192.png',
-						sizes: '192x192',
-						type: 'image/png',
-						purpose: 'any maskable'
-					},
-					{
-						src: '/icon-512x512.png',
-						sizes: '512x512',
-						type: 'image/png',
-						purpose: 'any maskable'
-					}
-				],
-				share_target: {
-					action: '/new',
-					method: 'GET',
-					params: {
-						title: 'title',
-						text: 'text',
-						url: 'link'
-					}
-				}
-			},
-			devOptions: {
-				enabled: true
-			}
-		})
-	]
+	plugins: [sveltekit()]
 });
